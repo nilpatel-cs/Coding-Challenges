3590ea7c2da2acfe817f0f11845ecb86c9102ce4
Nil <Nil@Lightning.fios-router.home>
2019-02-16 11:37:21 -0500

    First Commit - Added basic sorting in which the minimum is move to the first spot and then the minimum of the remaining is moved to the second spot etc.

--- Challenges/src/FizzBuzz.java
+++ Challenges/src/FizzBuzz.java
@@ -11,11 +11,14 @@
  */
 public class FizzBuzz {
    public static void main(String[] args){
+   
    for(int i =1;i<=100;i++){
        if (i%3 == 0 && i%5 == 0) //made the first condition an "and" logic to eliminate multiples of three and five first added next line to improve clarity
            System.out.println("FizzBuzz, ");
        else if (i%3 == 0)
            System.out.println("Fizz, ");
+       else if (i == 100)
+           System.out.println("Buzz.");
        else if (i%5 == 0)
            System.out.println("Buzz, ");
        else
